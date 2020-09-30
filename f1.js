8c7ae0b9aac6d4bd1b0fd4bb64ce9963fc30fbb9
Kasina Suksila <60030007@kmitl.ac.th>
2020-09-30 15:09:19 +0700

    test1

--- f1.js
+++ f1.js
@@ -1,1 +1,6 @@
-console.log("Hello");
+var http = require('http');
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(8080);
