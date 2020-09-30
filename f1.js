0e052d935a421c7eb36b762f61c0320a4c1d25f1
Kasina Suksila <60030007@kmitl.ac.th>
2020-09-30 15:44:50 +0700

    test2

--- f1.js
+++ f1.js
@@ -1,5 +1,5 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
+  res.end('Hello Teacher My name is Kasina Suksila ');
 }).listen(3003);
